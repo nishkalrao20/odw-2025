598c43629178e04ac643317a8e59488d3705efa3
Mathieu Dubois <mathieu.dubois@l2it.in2p3.fr>
2025-02-17 11:55:17 +0100

    Add script and github action to check that notebook cells are in order

---
+++ tests/check_execution_order.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Check that the cells are executed in order
+# To do so we open the notebook and inspects the cells
+
+import argparse
+import json
+import sys
+import re
+import yaml
+
+
+# CLI
+parser = argparse.ArgumentParser(
+    description='Check that the cells are executed in order',
+)
+parser.add_argument('notebook')
+
+# Parse CLI
+args = parser.parse_args()
+fname = args.notebook
+
+# Check cell order
+errors = []
+with open(fname, "r") as file:
+    content = json.load(file)
+last_execution_count = 0
+for cell in content["cells"]:
+    try:
+        execution_count = cell["execution_count"]
+    except KeyError:
+        #print("Markdown cell", file=sys.stderr)
+        continue
+    #print(f"Expected: {last_execution_count+1}, Got: {execution_count}")
+    if execution_count != last_execution_count + 1:
+        msg = f"Cell {execution_count} should be {last_execution_count+1}"
+        errors.append(msg)
+    last_execution_count += 1
+
+if len(errors) > 0:
+    errors = "\n".join(errors)
+    msg =  f"Execution order issues in {fname}:\n"
+    msg += f"{errors}"
+    print(msg)
+    sys.exit(1)
+else:
+    print(f"No execution order issue in {fname}")
+    sys.exit(0)
